5719f648ba5a308d1489d77b2ff671bf899dcde5
jinyongp <dev.jinyongp@gmail.com>
2022-03-03 22:16:58 +0900

    Feat: TabIcon 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
+import TabIcon from './components/TabIcon';
 import IssueScreen from './screens/Issue';
 import MyRepoScreen from './screens/MyRepo';
 import SearchScreen from './screens/Search';
@@ -11,10 +12,33 @@ export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        <Tabs.Navigator>
-          <Tabs.Screen name="Search" component={SearchScreen} />
-          <Tabs.Screen name="Issue" component={IssueScreen} />
-          <Tabs.Screen name="MyRepo" component={MyRepoScreen} />
+        <Tabs.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+          }}
+        >
+          <Tabs.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{
+              tabBarIcon: (props) => <TabIcon name="repo" {...props} />,
+            }}
+          />
+          <Tabs.Screen
+            name="Issue"
+            component={IssueScreen}
+            options={{
+              tabBarIcon: (props) => <TabIcon name="issue-opened" {...props} />,
+            }}
+          />
+          <Tabs.Screen
+            name="MyRepo"
+            component={MyRepoScreen}
+            options={{
+              tabBarIcon: (props) => <TabIcon name="heart" {...props} />,
+            }}
+          />
         </Tabs.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
