9fb5f8997ecf6233bcfba524fdb6d7d8770658fc
jinyongp <dev.jinyongp@gmail.com>
2022-03-03 18:17:42 +0900

    Feat: BottomTabs 추가
    - Search: 저장소 검색
    - Issue: 이슈 리스트 확인
    - MyRepo: 등록한 저장소 목록

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
+import IssueScreen from './screens/Issue';
+import MyRepoScreen from './screens/MyRepo';
 import SearchScreen from './screens/Search';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <SearchScreen />
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Tabs.Navigator>
+          <Tabs.Screen name="Search" component={SearchScreen} />
+          <Tabs.Screen name="Issue" component={IssueScreen} />
+          <Tabs.Screen name="MyRepo" component={MyRepoScreen} />
+        </Tabs.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
