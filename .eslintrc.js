9ae0b2260d68c0698f3517fcc2f301350c671227
jinyongp <dev.jinyongp@gmail.com>
2022-03-03 14:23:10 +0900

    Init: 프로젝트 초기화

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
