e52ff01209b978cd7b842fb05e2ae60d2ce59258
jinyongp <dev.jinyongp@gmail.com>
2022-03-04 01:04:28 +0900

    Feat: isShrink prop 추가로 true일 때 작아지는 효과 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,44 @@
 import { Box, Heading } from 'native-base';
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
+import { Animated } from 'react-native';
 
-export default function Header({ children }) {
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+export default function Header({ children, isShrink = false }) {
+  const boxHeight = useRef(new Animated.Value(150)).current;
+
+  useEffect(() => {
+    if (isShrink) {
+      Animated.timing(boxHeight, {
+        toValue: 50,
+        duration: 100,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(boxHeight, {
+        toValue: 150,
+        duration: 100,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [isShrink, boxHeight]);
+
   return (
-    <Box
+    <AnimatedBox
       shadow="2"
-      height="40"
       justifyContent="center"
       alignItems="center"
       bg="dark.100"
       borderRadius="20"
+      style={{ height: boxHeight }}
     >
       <Heading color="gray.200">{children}</Heading>
-    </Box>
+    </AnimatedBox>
   );
 }
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
+  isShrink: PropTypes.bool,
 };
