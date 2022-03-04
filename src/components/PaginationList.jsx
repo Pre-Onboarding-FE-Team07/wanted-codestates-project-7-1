79e220a211bb635fa4f800a15e668ce789b272b1
jinyongp <dev.jinyongp@gmail.com>
2022-03-04 16:25:51 +0900

    Feat: Pagination 추가

--- src/components/PaginationList.jsx
+++ src/components/PaginationList.jsx
@@ -1,6 +1,7 @@
 import { FlatList } from 'native-base';
 import PropTypes from 'prop-types';
 import { cloneElement } from 'react';
+import Pagination from './Pagination';
 
 export default function PaginationList({
   currentPage = 1,
@@ -12,6 +13,13 @@ export default function PaginationList({
   return (
     <FlatList
       data={data}
+      ListHeaderComponent={
+        <Pagination
+          numberOfPages={numberOfPages}
+          currentPage={currentPage}
+          onChange={onChange}
+        />
+      }
       stickyHeaderIndices={[0]}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => cloneElement(renderItem(item))}
@@ -23,6 +31,6 @@ PaginationList.propTypes = {
   currentPage: PropTypes.number,
   numberOfPages: PropTypes.number,
   onChange: PropTypes.func,
-  data: PropTypes.array,
-  renderItem: PropTypes.func,
+  data: PropTypes.array.isRequired,
+  renderItem: PropTypes.func.isRequired,
 };
